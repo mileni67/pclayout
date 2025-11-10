8ca4750d0744f3519db480ce2085a6eb9c9c366b
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-11-10 13:09:56 +0100

    rename file

---
+++ js/accordeon.js
@@ -0,0 +1,16 @@
+const contents = document.querySelectorAll(".program-line__content");
+
+contents.forEach((elem) => {
+  const title = elem.querySelector(".program-line__title");
+  const descr = elem.querySelector(".program-line__descr");
+
+  title.addEventListener("click", () => {
+    contents.forEach((item) => {
+      const otherDescr = item.querySelector(".program-line__descr");
+      if (otherDescr !== descr) {
+        otherDescr.classList.remove("active");
+      }
+    });
+    descr.classList.toggle("active");
+  });
+});
